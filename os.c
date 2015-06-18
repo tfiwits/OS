beb59b8a8f671a7146d92688d76c67e961823c51
tfiwits <lp332333@gmail.com>
2015-06-18 13:10:57 +0800

    first in origin

--- os.c
+++ os.c
@@ -6,36 +6,33 @@ typedef struct work{
 	int randInt;
 	int randTime;
 }sW;
-typedef struct workBlock{
-	int workSize;
-	int freeSize;
-	int cc;
-}wB;
+
 typedef struct node {
 	int workSize;
 	int freeSize;
-	int cc;
 	int addr;
+	int timed;
 	struct node *left;
 	struct node *right;
 }node;
+
+
 node* add(node* top, int item) {
 	node* newnode;
 
 	newnode = (node*)malloc(sizeof(node));
 
 	if (newnode == NULL) {
-		printf("\\n記憶體配置失敗！");
+		printf("記憶體配置失敗\n");
 		exit(1);
 	}
 
 	newnode->freeSize = item;
-	printf("fun%d\n",newnode->freeSize);
+	/*printf("fun%d\n",newnode->freeSize);*/
 	newnode->left = top;
 	newnode->right = top->right;
 	top->right = newnode;
 	return newnode;
-
 }
 
 /*node* delete(node* top) {*/
@@ -56,7 +53,7 @@ node* add(node* top, int item) {
 void PrintList(node *head)
 {
 	node *p=head;
-	printf("顯示");
+	printf("工作區塊\n");
 	for(p;p->left!=0;p=p->left)
 		printf("%d\n",p->freeSize);
 }
@@ -81,129 +78,116 @@ int main(int argc, char *argv[])
 		printf("請選擇模式(1)FIFO(2)最適(3)最不適(其他)離開：");
 		scanf("%d",&switchMode);
 		printf("記憶體區塊\n");
-		/*for (int i = 0; i < randWork*2 + 1; ++i)*/
-		/*{*/
-				/*workB.freeSize = 0;*/
-				/*workB.workSize = 0;*/
-				/*workB.cc = 0;*/
-		/*}*/
 		for (int i = 0; i < randWork ; ++i)
 		{
 			randFull = (switchMerroy - switchOSSize) / randWork;
 			randWorkSize = (rand() % randFull) + 1;
-			printf("rand%d\n",randWorkSize);
 			workB = add(workB,randWorkSize);
-		printf("%d,%d\n",workB->freeSize,(randFull - randWorkSize));
+			workCount += workB->freeSize;
 			workB = add(workB,(randFull - workB->freeSize));
-		printf("%d\n",workB->freeSize);
-			/*workCount += workB[i].freeSize + workB[i*2].freeSize;*/
+			workCount += workB->freeSize;
 		}
-		/*printf("%d ",workB->freeSize);*/
 		PrintList(workB);
-		/*for (int i = 0; i < randWork*2; ++i)*/
-		/*{*/
-			/*printf("%d.%d-%d\n",i,workB[i].freeSize,workB[i].cc);*/
-		/*}*/
+		if (switchMerroy - switchOSSize - workCount != 0 && !(switchMerroy - switchOSSize - workCount < 0))
+		{
+			workB = add(workB,switchMerroy - switchOSSize - workCount);
+			printf("%d\n",workB->freeSize);
+		}
 		printf("記憶體大小:%d\n",switchMerroy);
 		printf("os佔的:%d\n",switchOSSize);
-		/*printf("總%d\n",workCount);*/
-		/*if (switchMerroy - switchOSSize - workCount != 0 && !(switchMerroy - switchOSSize - workCount < 0))*/
-		/*{*/
-			/*workB[randWork*2].freeSize = switchMerroy - switchOSSize - workCount;*/
-			/*printf("最後%d\n",workB[randWork*2].freeSize);*/
-		/*}*/
+		printf("總%d\n",workCount);
 		printf("工作\n");
 		for (int i = 0; i < switchArray; ++i)
 		{
-			workInt[i].randInt=(rand() % (switchMerroy-switchOSSize) + 1) / randWork;
+			workInt[i].randInt=(rand() % (switchMerroy-switchOSSize) + 2) / randWork;
 			workInt[i].randTime=(rand() % 15) + 1;
 			printf("%d.%d\t%d\n",i,workInt[i].randInt,workInt[i].randTime);
 		}
 		
 		workMem = switchOSSize;
-		/*switch (switchMode) {*/
-				/*case 1://FIFO*/
-							/*for (int i = 0; i < switchArray; ++i){*/
-									/*for (int j = 0; j < randWork * 2 + 1 ; ++j)*/
+		switch (switchMode) {
+				case 1:
+							for (int i = 0; i < switchArray; ++i){
+									for (workB;workB->left!=0;workB=workB->left)
+									{
+										if (workInt[i].randInt <= workB->freeSize)
+										{
+											printf("工作區塊大小%d\n",workB->freeSize );
+											workB->freeSize = workB->freeSize -  workInt[i].randInt;
+											workB->workSize = workInt[i].randInt;
+											printf("工作%d放到剩餘%d區塊大小%d工作時間為%d\n\n",i,workB->freeSize,workB->workSize,workInt[i].randTime);
+											workInt[i].randInt = 0;
+											break;
+										}
+									}
+						}
+							for (int i = 0; i < switchArray; ++i){
+								if (workInt[i].randInt !=0)
+								{
+										printf("記憶體不足,工作%d無法執行\n",i);
+								}
+							}
+									/*for (int i = 0;  i < randWork*2 + 1; ++i)*/
 									/*{*/
-										/*if (workInt[i].randInt <= workB[j].freeSize)*/
-										/*{*/
-											/*printf("工作區塊大小%d\n",workB[j].freeSize );*/
-											/*workB[j].freeSize = workB[j].freeSize -  workInt[i].randInt;*/
-											/*workB[j].workSize = workInt[i].randInt;*/
-											/*printf("工作%d放到%d剩餘%d區塊大小%d工作時間為%d\n\n",i,j,workB[j].freeSize,workB[j].workSize,workInt[i].randTime);*/
-											/*workInt[i].randInt = 0;*/
-											/*break;*/
-										/*}*/
+											/*if (workB[i].workSize != 0)*/
+											/*{*/
+													/*printf("區塊%d內工作執行結束\n",i );*/
+													/*workB[i].freeSize+= workB[i].workSize;*/
+											/*}*/
 									/*}*/
-						/*}*/
-							/*for (int i = 0; i < switchArray; ++i){*/
-								/*if (workInt[i].randInt !=0)*/
-								/*{*/
-										/*printf("記憶體不足,工作%d無法執行\n",i);*/
-								/*}*/
-							/*}*/
-									/*[>for (int i = 0;  i < randWork*2 + 1; ++i)<]*/
-									/*[>{<]*/
-											/*[>if (workB[i].workSize != 0)<]*/
-											/*[>{<]*/
-													/*[>printf("區塊%d內工作執行結束\n",i );<]*/
-													/*[>workB[i].freeSize+= workB[i].workSize;<]*/
-											/*[>}<]*/
-									/*[>}<]*/
-					/*break;*/
-				/*case 2://最適*/
-					/*for (int i = 0; i < randWork * 2; i++)*/
-						/*for (int j = 0; j < randWork * 2 - i; j++)*/
-						/*{*/
-								/*if(workB[j].freeSize>workB[j+1].freeSize){*/
-									/*tmp = workB[j].freeSize;*/
-									/*workB[j].freeSize =workB[j+1].freeSize;*/
-									/*workB[j+1].freeSize = tmp;*/
-									/*cctmp = workB[j].cc;*/
-									/*workB[j].cc =workB[j+1].cc;*/
-									/*workB[j+1].cc= cctmp;*/
-								/*}*/
-						/*}*/
-					 /*for (int i = 0; i < randWork*2+1; ++i)*/
-					  /*{*/
-						  /*printf("%d.%d-%d\n",i,workB[i].freeSize,workB[i].cc);*/
-					/*}*/
-							/*for (int i = 0; i < switchArray; ++i){*/
-									/*for (int j = 0; j < randWork * 2 + 1 ; ++j)*/
-									/*{*/
-										/*if (workInt[i].randInt <= workB[j].freeSize)*/
-										/*{*/
-											/*printf("工作區塊大小%d\n",workB[j].freeSize );*/
-											/*workB[j].freeSize = workB[j].freeSize -  workInt[i].randInt;*/
-											/*workB[j].workSize = workInt[i].randInt;*/
-											/*printf("工作%d放到%d剩餘%d工作大小%d工作時間為%d\n\n",i,workB[j].cc,workB[j].freeSize,workB[j].workSize,workInt[i].randTime);*/
-											/*workInt[i].randInt = 0;*/
-					/*for (int i = 0; i < randWork * 2; i++)*/
-						/*for (int j = 0; j < randWork * 2 - i; j++)*/
-						/*{*/
-								/*if(workB[j].freeSize>workB[j+1].freeSize){*/
-									/*tmp = workB[j].freeSize;*/
-									/*workB[j].freeSize =workB[j+1].freeSize;*/
-									/*workB[j+1].freeSize = tmp;*/
-									/*cctmp = workB[j].cc;*/
-									/*workB[j].cc =workB[j+1].cc;*/
-									/*workB[j+1].cc= cctmp;*/
-								/*}*/
-						/*}*/
-											/*break;*/
-										/*}*/
-									/*}*/
-						/*}*/
-							/*for (int i = 0; i < switchArray; ++i){*/
-								/*if (workInt[i].randInt !=0)*/
-								/*{*/
-										/*printf("記憶體不足,工作%d無法執行\n",i);*/
-								/*}*/
-							/*}*/
+					break;
+				case 2:
+					for (int i = 0; i < randWork * 2; i++)
+						for (int j = 0; j < randWork * 2 - i; j++)
+						{
+								if(workB[j].freeSize>workB[j+1].freeSize){
+									tmp = workB[j].freeSize;
+									workB[j].freeSize =workB[j+1].freeSize;
+									workB[j+1].freeSize = tmp;
+									cctmp = workB[j].cc;
+									workB[j].cc =workB[j+1].cc;
+									workB[j+1].cc= cctmp;
+								}
+						}
+					 for (int i = 0; i < randWork*2+1; ++i)
+					  {
+						  printf("%d.%d-%d\n",i,workB[i].freeSize,workB[i].cc);
+					}
+							for (int i = 0; i < switchArray; ++i){
+									for (int j = 0; j < randWork * 2 + 1 ; ++j)
+									{
+										if (workInt[i].randInt <= workB[j].freeSize)
+										{
+											printf("工作區塊大小%d\n",workB[j].freeSize );
+											workB[j].freeSize = workB[j].freeSize -  workInt[i].randInt;
+											workB[j].workSize = workInt[i].randInt;
+											printf("工作%d放到%d剩餘%d工作大小%d工作時間為%d\n\n",i,workB[j].cc,workB[j].freeSize,workB[j].workSize,workInt[i].randTime);
+											workInt[i].randInt = 0;
+					for (int i = 0; i < randWork * 2; i++)
+						for (int j = 0; j < randWork * 2 - i; j++)
+						{
+								if(workB[j].freeSize>workB[j+1].freeSize){
+									tmp = workB[j].freeSize;
+									workB[j].freeSize =workB[j+1].freeSize;
+									workB[j+1].freeSize = tmp;
+									cctmp = workB[j].cc;
+									workB[j].cc =workB[j+1].cc;
+									workB[j+1].cc= cctmp;
+								}
+						}
+											break;
+										}
+									}
+						}
+							for (int i = 0; i < switchArray; ++i){
+								if (workInt[i].randInt !=0)
+								{
+										printf("記憶體不足,工作%d無法執行\n",i);
+								}
+							}
 
-					/*break;*/
-				/*case 3://最不適*/
+					break;
+				case 3:
 					/*for (int i = 0; i < randWork * 2; i++)*/
 						/*for (int j = 0; j < randWork * 2 - i; j++)*/
 						/*{*/
@@ -253,9 +237,9 @@ int main(int argc, char *argv[])
 								/*}*/
 							/*}*/
 
-					/*break;*/
-				/*default:*/
-					/*printf("exit\n");*/
-		/*}*/
+					break;
+				default:
+					printf("exit\n");
+		}
 		return 0;
 }
